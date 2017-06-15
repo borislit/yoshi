79739e447af8fdbedc153d0ed75e6a058e3834fa
Audrius Siliunas <audriuss@wix.com>
2017-06-15 14:40:47 +0300

    improve(copy): do not use gulp

--- plugins/yoshi-copy/index.js
+++ plugins/yoshi-copy/index.js
@@ -1,15 +1,49 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs');
 const gulp = require('gulp');
+const glob = require('glob');
+const mkdirp = require('mkdirp');
 
-function copyDir(source, destination = '', base = '.') {
+
+function copyFiles(patterns, destination = '', base = '.') {
+  return Promise.all(patterns.map(pattern =>
+    copy(pattern, destination, path.resolve(base))));
+}
+
+function copy(pattern, destination, base) {
+  return matchFiles(path.resolve(pattern)).then(matches =>
+    Promise.all(matches.map(match => {
+      const dest = path.resolve('dist', destination);
+      const to = path.join(dest, match.replace(base, ''));
+      return copyFile(match, to);
+    })));
+}
+
+function matchFiles(pattern) {
+  return new Promise((resolve, reject) => {
+    glob(pattern, (err, matches) => err ? reject(err) : resolve(matches));
+  });
+}
+
+function copyFile(from, to) {
+  return ensureDir(path.dirname(to)).then(() => {
+    return new Promise((resolve, reject) => {
+      const readStream = fs.createReadStream(from);
+      const writeStream = fs.createWriteStream(to);
+
+      readStream.on('error', reject);
+      writeStream.on('error', reject);
+      writeStream.once('finish', resolve);
+      readStream.pipe(writeStream);
+    });
+  });
+}
+
+function ensureDir(dir) {
   return new Promise((resolve, reject) =>
-    gulp.src(source, {base})
-      .pipe(gulp.dest(path.join('dist', destination)))
-      .on('error', reject)
-      .once('end', resolve)
-  );
+    mkdirp(dir, err => err ? reject(err) : resolve(dir)));
 }
 
 module.exports = ({log, watch, base}) => {
@@ -19,8 +53,8 @@ module.exports = ({log, watch, base}) => {
     const serverAssets = `${base()}/**/*.{css,json,d.ts}`;
 
     const copyAllAssets = () => Promise.all([
-      copyDir([assets, htmlAssets, serverAssets]),
-      copyDir([assets, htmlAssets], output, path.join(process.cwd(), 'src'))
+      copyFiles([assets, htmlAssets, serverAssets]),
+      copyFiles([assets, htmlAssets], output, path.join(process.cwd(), 'src'))
     ]);
 
     if (watch) {
